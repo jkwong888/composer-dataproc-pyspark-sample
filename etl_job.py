f9af6cac60dbdf12d28e3a63146563e1e173e281
Jeffrey Kwong <jkwng@google.com>
2020-02-15 15:15:42 -0500

    simplify job

---
+++ etl_job.py
@@ -0,0 +1,266 @@
+"""
+etl_job.py
+~~~~~~~~~~
+
+This Python module contains an example Apache Spark ETL job definition
+that implements best practices for production ETL jobs. It can be
+submitted to a Spark cluster (or locally) using the 'spark-submit'
+command found in the '/bin' directory of all Spark distributions
+(necessary for running any Spark job, locally or otherwise). For
+example, this example script can be executed as follows,
+
+    $SPARK_HOME/bin/spark-submit \
+    --master spark://localhost:7077 \
+    --py-files packages.zip \
+    --files configs/etl_config.json \
+    jobs/etl_job.py
+
+where packages.zip contains Python modules required by ETL job (in
+this example it contains a class to provide access to Spark's logger),
+which need to be made available to each executor process on every node
+in the cluster; etl_config.json is a text file sent to the cluster,
+containing a JSON object with all of the configuration parameters
+required by the ETL job; and, etl_job.py contains the Spark application
+to be executed by a driver process on the Spark master node.
+
+For more details on submitting Spark applications, please see here:
+http://spark.apache.org/docs/latest/submitting-applications.html
+
+Our chosen approach for structuring jobs is to separate the individual
+'units' of ETL - the Extract, Transform and Load parts - into dedicated
+functions, such that the key Transform steps can be covered by tests
+and jobs or called from within another environment (e.g. a Jupyter or
+Zeppelin notebook).
+"""
+
+import __main__
+from os import environ, listdir, path
+import json
+from pyspark import SparkFiles
+from pyspark.sql import SparkSession
+import argparse
+
+from pyspark.sql import Row
+from pyspark.sql.functions import col, concat_ws, lit
+
+class Log4j(object):
+    """Wrapper class for Log4j JVM object.
+
+    :param spark: SparkSession object.
+    """
+
+    def __init__(self, spark):
+        # get spark app details with which to prefix all messages
+        conf = spark.sparkContext.getConf()
+        app_id = conf.get('spark.app.id')
+        app_name = conf.get('spark.app.name')
+
+        log4j = spark._jvm.org.apache.log4j
+        message_prefix = '<' + app_name + ' ' + app_id + '>'
+        self.logger = log4j.LogManager.getLogger(message_prefix)
+
+    def error(self, message):
+        """Log an error.
+
+        :param: Error message to write to log
+        :return: None
+        """
+        self.logger.error(message)
+        return None
+
+    def warn(self, message):
+        """Log an warning.
+
+        :param: Error message to write to log
+        :return: None
+        """
+        self.logger.warn(message)
+        return None
+
+    def info(self, message):
+        """Log information.
+
+        :param: Information message to write to log
+        :return: None
+        """
+        self.logger.info(message)
+        return None
+
+def start_spark(app_name='my_spark_app', master='local[*]', jar_packages=[],
+                files=[], spark_config={}):
+    """Start Spark session, get Spark logger and load config files.
+
+    Start a Spark session on the worker node and register the Spark
+    application with the cluster. Note, that only the app_name argument
+    will apply when this is called from a script sent to spark-submit.
+    All other arguments exist solely for testing the script from within
+    an interactive Python console.
+
+    This function also looks for a file ending in 'config.json' that
+    can be sent with the Spark job. If it is found, it is opened,
+    the contents parsed (assuming it contains valid JSON for the ETL job
+    configuration) into a dict of ETL job configuration parameters,
+    which are returned as the last element in the tuple returned by
+    this function. If the file cannot be found then the return tuple
+    only contains the Spark session and Spark logger objects and None
+    for config.
+
+    The function checks the enclosing environment to see if it is being
+    run from inside an interactive console session or from an
+    environment which has a `DEBUG` environment variable set (e.g.
+    setting `DEBUG=1` as an environment variable as part of a debug
+    configuration within an IDE such as Visual Studio Code or PyCharm.
+    In this scenario, the function uses all available function arguments
+    to start a PySpark driver from the local PySpark package as opposed
+    to using the spark-submit and Spark cluster defaults. This will also
+    use local module imports, as opposed to those in the zip archive
+    sent to spark via the --py-files flag in spark-submit.
+
+    :param app_name: Name of Spark app.
+    :param master: Cluster connection details (defaults to local[*]).
+    :param jar_packages: List of Spark JAR package names.
+    :param files: List of files to send to Spark cluster (master and
+        workers).
+    :param spark_config: Dictionary of config key-value pairs.
+    :return: A tuple of references to the Spark session, logger and
+        config dict (only if available).
+    """
+
+    # detect execution environment
+    flag_repl = not(hasattr(__main__, '__file__'))
+    flag_debug = 'DEBUG' in environ.keys()
+
+    if not (flag_repl or flag_debug):
+        # get Spark session factory
+        spark_builder = (
+            SparkSession
+            .builder
+            .appName(app_name))
+    else:
+        # get Spark session factory
+        spark_builder = (
+            SparkSession
+            .builder
+            .master(master)
+            .appName(app_name))
+
+        # create Spark JAR packages string
+        spark_jars_packages = ','.join(list(jar_packages))
+        spark_builder.config('spark.jars.packages', spark_jars_packages)
+
+        spark_files = ','.join(list(files))
+        spark_builder.config('spark.files', spark_files)
+
+        # add other config params
+        for key, val in spark_config.items():
+            spark_builder.config(key, val)
+
+    # create session and retrieve Spark logger object
+    spark_sess = spark_builder.getOrCreate()
+    spark_logger = Log4j(spark_sess)
+
+    # get config file if sent to cluster with --files
+    spark_files_dir = SparkFiles.getRootDirectory()
+    config_files = [filename
+                    for filename in listdir(spark_files_dir)
+                    if filename.endswith('config.json')]
+
+    if config_files:
+        path_to_config_file = path.join(spark_files_dir, config_files[0])
+        with open(path_to_config_file, 'r') as config_file:
+            config_dict = json.load(config_file)
+        spark_logger.warn('loaded config from ' + config_files[0])
+    else:
+        spark_logger.warn('no config file found')
+        config_dict = None
+
+    return spark_sess, spark_logger, config_dict
+
+
+def main():
+    """Main ETL script definition.
+
+    :return: None
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', help="input file", nargs='?', required=True)
+    parser.add_argument('-o', '--output', help="output_file", nargs='?', required=True)
+    args = parser.parse_args()
+
+    if args.input:
+        inputfile = args.input
+    
+    if args.output:
+        outputfile = args.output
+
+    # start Spark application and get Spark session, logger and config
+    spark, log, config = start_spark(
+        app_name='daily_show_guests_sparksql')
+
+    # log that main ETL job is starting
+    log.warn('etl_job is up-and-running')
+    log.info('input: %s' % inputfile)
+    log.info('output: %s' % outputfile)
+
+    if inputfile.startswith('gs://') or outputfile.startswith('gs://'):
+        spark.conf.set('fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem')
+        spark.conf.set('fs.AbstractFileSystem.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS')
+
+    # execute ETL pipeline
+    data = extract_data(spark, inputfile)
+    data_transformed = transform_data(spark, data)
+    load_data(data_transformed, outputfile)
+
+    # log the success and terminate Spark application
+    log.warn('etl_job is finished')
+    spark.stop()
+    return None
+
+
+def extract_data(spark, inputfile):
+    """Load data from Parquet file format.
+
+    :param spark: Spark session object.
+    :return: Spark DataFrame.
+    """
+    df = (
+        spark
+        .read
+        .format('csv')
+        .options(header='true', inferSchema='true')
+        .load(inputfile))
+
+    return df
+
+
+def transform_data(spark, df):
+    """Transform original dataset.
+
+    :param df: Input DataFrame.
+    :param steps_per_floor_: The number of steps per-floor at 43 Tanner
+        Street.
+    :return: Transformed DataFrame.
+    """
+
+    df.createOrReplaceTempView('guests')
+    df_transformed = spark.sql("SELECT YEAR, Group, COUNT(Group) FROM guests GROUP BY YEAR, Group ORDER BY YEAR")
+    return df_transformed
+
+
+def load_data(df, outputfile):
+    """Collect data locally and write to CSV.
+
+    :param df: DataFrame to print.
+    :return: None
+    """
+    (df
+     .write
+     .mode("overwrite")
+     .csv(outputfile))
+    return None
+
+
+# entry point for PySpark ETL application
+if __name__ == '__main__':
+    main()
